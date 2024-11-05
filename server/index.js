b7b97c9f6d9b8cb3cef221d02de92cb3176fa860
Prerit Khandelwal <prerit.web@gmail.com>
2024-11-05 14:40:56 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const { express } = require('express');
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require('@apollo/server/express4');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({});
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await server.start()
+
+    app.use('graphql', expressMiddleware(server));
+
+
+}
