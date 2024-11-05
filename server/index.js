404ade43429f615f8fcdec85f680b13579978cbc
Prerit Khandelwal <prerit.web@gmail.com>
2024-11-05 17:59:34 +0530

    Mutation Implemented

--- server/index.js
+++ server/index.js
@@ -3,41 +3,87 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require('@apollo/server/express4');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { default: axios } = require('axios');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const TodoSchema = new mongoose.Schema({
+    title: String,
+    completed: Boolean,
+    userId: mongoose.Schema.Types.ObjectId,
+});
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+const Todo = mongoose.model('Todo', TodoSchema);
+const User = mongoose.model('User', UserSchema);
 
 async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
             type User {
-                id:ID!
-                name:String
-                email:String
+                id: ID!
+                name: String
+                email: String
             }
            type Todo {
                 id: ID!
                 title: String!
                 completed: Boolean
-                user:User
+                user: User
             }
 
             type Query {
                 getTodos: [Todo!]!
-                getAllUsers:[User]!
-                getUserById(id:ID!):User
+                getAllUsers: [User!]!
+                getUserById(id: ID!): User
+            }
+
+            type Mutation {
+                createTodo(title: String!, completed: Boolean, userId: ID!): Todo!
+                createUser(name: String!, email: String!): User!
+                updateTodo(id: ID!, title: String, completed: Boolean): Todo
+                deleteTodo(id: ID!): Boolean
             }
         `,
         resolvers: {
             Todo: {
-                user: async (todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.id}`)).data
+                user: async (todo) => await User.findById(todo.userId),
             },
             Query: {
-                getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
-                getAllUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
-                getUserById: async (parent, { id }) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data
-
-            }
-        }
+                getTodos: async () => await Todo.find(),
+                getAllUsers: async () => await User.find(),
+                getUserById: async (parent, { id }) => await User.findById(id),
+            },
+            Mutation: {
+                createTodo: async (parent, { title, completed, userId }) => {
+                    const todo = new Todo({
+                        title,
+                        completed,
+                        userId,
+                    });
+                    return await todo.save();
+                },
+                createUser: async (parent, { name, email }) => {
+                    const user = new User({ name, email });
+                    return await user.save();
+                },
+                updateTodo: async (parent, { id, title, completed }) => {
+                    return await Todo.findByIdAndUpdate(id, { title, completed }, { new: true });
+                },
+                deleteTodo: async (parent, { id }) => {
+                    const result = await Todo.findByIdAndDelete(id);
+                    return !!result;
+                },
+            },
+        },
     });
 
     app.use(bodyParser.json());
@@ -45,7 +91,7 @@ async function startServer() {
 
     await server.start();
 
-    app.use('/graphql', expressMiddleware(server)); // Note the '/' in '/graphql'
+    app.use('/graphql', expressMiddleware(server));
 
     app.listen(8000, () => console.log(`Server is started at PORT: 8000`));
 }
